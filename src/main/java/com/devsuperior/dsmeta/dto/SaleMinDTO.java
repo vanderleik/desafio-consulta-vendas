d6fa7f4e37e1fdd3e2506f1daa08969a598a14a8
vanderleik <vanderleik@yahoo.com.br>
2025-04-18 10:13:56 -0300

    feat: adicionando campo sellerName no DTO

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -1,25 +1,28 @@
 package com.devsuperior.dsmeta.dto;
 
-import java.time.LocalDate;
-
 import com.devsuperior.dsmeta.entities.Sale;
+
+import java.time.LocalDate;
 
 public class SaleMinDTO {
 
 	private Long id;
 	private Double amount;
 	private LocalDate date;
-	
-	public SaleMinDTO(Long id, Double amount, LocalDate date) {
+	private String sellerName;
+
+	public SaleMinDTO(Long id, Double amount, LocalDate date, String sellerName) {
 		this.id = id;
 		this.amount = amount;
 		this.date = date;
+		this.sellerName = sellerName;
 	}
 	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		sellerName = entity.getSeller().getName();
 	}
 
 	public Long getId() {
@@ -32,5 +35,9 @@ public class SaleMinDTO {
 
 	public LocalDate getDate() {
 		return date;
+	}
+
+	public String getSellerName() {
+		return sellerName;
 	}
 }
