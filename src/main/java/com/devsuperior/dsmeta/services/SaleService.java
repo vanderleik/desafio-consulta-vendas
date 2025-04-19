efc5cab72268cf2b762125c4469744b289f2bdae
vanderleik <vanderleik@yahoo.com.br>
2025-04-19 09:00:24 -0300

    feat: criando método getSummary no Service

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsmeta.services;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSumaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.data.domain.Page;
@@ -17,6 +18,7 @@ import java.util.Optional;
 @Service
 public class SaleService {
 
+	public static final LocalDate TODAY = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 	private final SaleRepository repository;
 
     public SaleService(SaleRepository repository) {
@@ -25,14 +27,15 @@ public class SaleService {
 
     public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
+		if (result.isEmpty()) {
+			return null;
+		}
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
 
 	public Page<SaleReportDTO> getReport(String minDate, String maxDate, String name, Pageable pageable) {
-		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-
-		LocalDate max = (maxDate == null || maxDate.isBlank()) ? today : LocalDate.parse(maxDate, DateTimeFormatter.ISO_DATE);
+		LocalDate max = (maxDate == null || maxDate.isBlank()) ? TODAY : LocalDate.parse(maxDate, DateTimeFormatter.ISO_DATE);
 		LocalDate min = (minDate == null || minDate.isBlank()) ? max.minusYears(1) : LocalDate.parse(minDate, DateTimeFormatter.ISO_DATE);
 		String sellerName = (name == null || name.isBlank()) ? "" : name;
 
@@ -40,8 +43,10 @@ public class SaleService {
 		return result.map(SaleReportDTO::new);
 	}
 
-	public Page<SaleMinDTO> getSummary(String minDate, String maxDate) {
+	public Page<SaleSumaryDTO> getSummary(String minDate, String maxDate, Pageable pageable) {
+		LocalDate max = (maxDate == null || maxDate.isBlank()) ? TODAY : LocalDate.parse(maxDate, DateTimeFormatter.ISO_DATE);
+		LocalDate min = (minDate == null || minDate.isBlank()) ? max.minusYears(1) : LocalDate.parse(minDate, DateTimeFormatter.ISO_DATE);
 
-		return null;
+		return repository.getSalesSummary(min, max, pageable);
 	}
 }
