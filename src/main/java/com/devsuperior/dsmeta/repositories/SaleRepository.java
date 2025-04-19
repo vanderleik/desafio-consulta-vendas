f4f33f847a507180b38480fff7a93c4186fdcf21
vanderleik <vanderleik@yahoo.com.br>
2025-04-19 08:59:58 -0300

    feat: criando getSalesSummary no Repository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.dto.SaleSumaryDTO;
 
 import java.time.LocalDate;
 
@@ -14,5 +15,8 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
 	@Query("SELECT s FROM Sale s WHERE s.date BETWEEN :minDate AND :maxDate AND LOWER(s.seller.name) LIKE LOWER(CONCAT('%', :name, '%'))")
 	Page<Sale> findSales(@Param("minDate") LocalDate minDate, @Param("maxDate") LocalDate maxDate, @Param("name") String name, Pageable pageable);
-}
 
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleSumaryDTO(s.seller.name, SUM(s.amount)) " +
+	       "FROM Sale s WHERE s.date BETWEEN :minDate AND :maxDate GROUP BY s.seller.name")
+	Page<SaleSumaryDTO> getSalesSummary(@Param("minDate") LocalDate minDate, @Param("maxDate") LocalDate maxDate, Pageable pageable);
+}
