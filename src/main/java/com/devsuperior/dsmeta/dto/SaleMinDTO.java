44a87d630c2990c345ac23170efa5c5f1e192b49
vanderleik <vanderleik@yahoo.com.br>
2025-04-19 08:42:46 -0300

    feat: corrigindo SaleMinDTO

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -9,20 +9,17 @@ public class SaleMinDTO {
 	private Long id;
 	private Double amount;
 	private LocalDate date;
-	private String sellerName;
 
 	public SaleMinDTO(Long id, Double amount, LocalDate date, String sellerName) {
 		this.id = id;
 		this.amount = amount;
 		this.date = date;
-		this.sellerName = sellerName;
 	}
 	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
-		sellerName = entity.getSeller().getName();
 	}
 
 	public Long getId() {
@@ -37,7 +34,4 @@ public class SaleMinDTO {
 		return date;
 	}
 
-	public String getSellerName() {
-		return sellerName;
-	}
 }
